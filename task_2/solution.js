55d60725c39eb15ab5936d5899a21c24ab35426a
Slava Govako <Gavrila1992@gmail.com>
2022-11-20 11:09:46 +0300

    2/1

--- task_2/solution.js
+++ task_2/solution.js
@@ -11,8 +11,7 @@ function calcShipping(sum, min, shipping) {
     }
     if(productsSum >= freeShippingMinSum) {
         shippingSum = 0;
-    }
-    if((productsSum < freeShippingMinSum) & (productsSum > 0)) {
+    } else if((productsSum < freeShippingMinSum) && (productsSum > 0)) {
         shippingSum = shippingPrice;
     }
     
