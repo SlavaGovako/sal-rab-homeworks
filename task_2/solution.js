3494fc8684f89611b6f7dbbd99f1f62537eddd58
Slava Govako <Gavrila1992@gmail.com>
2022-11-20 10:12:03 +0300

    2.1

--- task_2/solution.js
+++ task_2/solution.js
@@ -5,16 +5,21 @@ function calcShipping(sum, min, shipping) {
 
     // Задание №2.1. Рассчитать доставку
 
-    // создайте переменную shippingSum
+    let shippingSum // создайте переменную shippingSum
 
-    // если productsSum равно 0,
-    // то shippingSum присвоить значение 0
+    if(productsSum = 0) {
+        let shippingSum = 0;
+    } // если productsSum равно 0, то shippingSum присвоить значение 0
 
-    // если productsSum Больше или равна freeShippingMinSum,
-    // то shippingSum присвоить значение 0
+    if(productsSum >= freeShippingMinSum) {
+        let shippingSum = 0;
+    }// если productsSum Больше или равна freeShippingMinSum, то shippingSum присвоить значение 0
+    
 
-    // если productsSum больше 0 и меньше freeShippingMinSum,
-    // то shippingSum присвоить значение shippingPrice
+    if(productsSum > 0 && < freeShippingMinSum) {
+        let shippingSum = shippingPrice;
+    }// если productsSum больше 0 и меньше freeShippingMinSum, то shippingSum присвоить значение shippingPrice
+    
 
     // Конец решения задания №2.1.
 
