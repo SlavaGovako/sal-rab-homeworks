b7b5279fd3e5d94cb52f6a80242959b61054a204
Slava Govako <Gavrila1992@gmail.com>
2022-11-20 13:09:17 +0300

    правки

--- task_3/solution.js
+++ task_3/solution.js
@@ -14,16 +14,23 @@ function renderCartItem(item) {
     // количество × цена ₽ = стоимость ₽
     // например,
     // 5 × 500.00 ₽ = 2500 ₽
-    let itemCountText = " "; // 1. Создайте переменную itemCountText, равную пустой строке
+    let itemCountText; // 1. Создайте переменную itemCountText, равную пустой строке
+    itemCountText = " ";
     itemCountText = itemCountText + product.count; // 2. Присвойте переменной itemCountText результат склеивания itemCountText и количества
-    itemCountText = itemCountText + ' × '; // 3. Присвойте переменной itemCountText результат склеивания itemCountText и ` × `
+    itemCountText = itemCountText + " × "; // 3. Присвойте переменной itemCountText результат склеивания itemCountText и ` × `
     itemCountText = itemCountText + product.price; // 4. Присвойте переменной itemCountText результат склеивания itemCountText и цены
-    itemCountText = itemCountText = ' ₽ = '; // 5. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽ = `
+    itemCountText = itemCountText = " ₽ = "; // 5. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽ = `
+    
     let sum = product.count * product.price; // 6. Создайте переменную sum и присвойте ей значение произведения количества и цены
     itemCountText = itemCountText + sum; // 7. Присвойте переменной itemCountText результат склеивания itemCountText и sum
-    itemCountText = itemCountText +  ' ₽'; // 8. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽`
+    itemCountText = itemCountText + " ₽"; // 8. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽`
 
     // Конец решения задания №3.1.
 
     return `<div class="cart-item"><div>${product.title}</div><div>${itemCountText}</div></div>`;
 }
+<div class="cart-item"><div>Пицца</div><div> ₽ = 2500 ₽</div></div>
+<div class="cart-item"><div>Пицца</div><div>5 × 500.00 ₽ = 2500 ₽</div></div>
+
+<div class="cart-item"><div>Эчпочмак</div><div> ₽ = 110 ₽</div></div>
+<div class="cart-item"><div>Эчпочмак</div><div>2 × 55.00 ₽ = 110 ₽</div></div>
