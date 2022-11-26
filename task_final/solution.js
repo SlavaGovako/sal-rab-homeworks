2b1fe67c8d391c3231bb317b331511ac3aa5db0c
Slava Govako <Gavrila1992@gmail.com>
2022-11-26 10:07:44 +0300

    коммит 1

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,12 +34,12 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
+    let data = {order: {address, sum}, client: {name, phone}, goods:[]};
 
     let countOfGoods = goods.length;
 
     for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+        data.goods.push({title:goods[i].title, count: goods[i].count});
     }
 
     data.order.address = address;
